9290ebb179590737396a62aabf81ca3823bc1ded
Chiajun Wang <ibara1454@gmail.com>
2024-04-06 18:37:47 +0900

    feat: remove legacy page and home page (#7)

--- tests/pages/legacy.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('screenshot test for /legacy', async ({ page }) => {
-  await page.goto('/legacy');
-  await expect(page).toHaveScreenshot('legacy.png', { fullPage: true });
-});
