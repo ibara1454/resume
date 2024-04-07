e2e0c11e5ba5b16de99d978b478493907680b6a3
Chiajun Wang <ibara1454@gmail.com>
2024-04-07 17:16:28 +0000

    chore: enable ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // Using Vite on Windows Subsystem for Linux (WSL) 2
 // https://vitejs.dev/config/server-options.html#server-watch
 // Note that usePolling leads to high CPU utilization.
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
@@ -23,4 +23,6 @@ export default defineConfig({
     enabled: false,
   },
   integrations: [react(), tailwind()],
+  output: 'server',
+  adapter: vercel(),
 });
