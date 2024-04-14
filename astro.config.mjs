7e64aedd74f563c495787fc9d1b694c51ca6cce2
Chiajun Wang <ibara1454@gmail.com>
2024-04-14 20:34:31 +0900

    chore: enable ssr (#13)
    * chore: enable ssr
    
    * chore: enable vercel image service
    
    * test: update snapshot

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // Using Vite on Windows Subsystem for Linux (WSL) 2
 // https://vitejs.dev/config/server-options.html#server-watch
 // Note that usePolling leads to high CPU utilization.
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
@@ -23,4 +23,11 @@ export default defineConfig({
     enabled: false,
   },
   integrations: [react(), tailwind()],
+  output: 'server',
+  adapter: vercel({
+    // Image service doesn't work by default, so if we didn't enable it,
+    // the requests sent by `<Image />` will result in 404 errors.
+    // https://stackoverflow.com/a/77738867
+    imageService: true,
+  }),
 });
