1d76c86a0ea0208e90a6e14cd584268c6a857a29
Chiajun Wang <ibara1454@gmail.com>
2024-04-21 02:54:23 +0900

    chore: setup eslint and type check (#16)
    * chore: setup eslint and type check
    
    * chore: add prettier script
    
    * chore: enable husky and lint-staged
    
    * chore: enable a11y eslint rules

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+// Copied from
+// https://github.com/ota-meshi/eslint-plugin-astro/blob/212e026b65626b9e15854b475c09f16194fcdd6f/README.md
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+export default [
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs['flat/recommended'],
+  ...eslintPluginAstro.configs['flat/jsx-a11y-recommended'],
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+    },
+  },
+];
